8c422e4fd8a1d6ee3a2bf70323c50bd9b915564d
Anton Yudin <alrami87@gmail.com>
2024-06-09 13:42:37 +0300

    Overload metods of class Calculator

--- src/itmo/java/basics/lesson2/Main.java
+++ src/itmo/java/basics/lesson2/Main.java
@@ -4,64 +4,63 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        //  Числа типа Integer
+        Calculator calculator = new Calculator();
+
+//  Числа типа Integer
         System.out.println();
         Integer intFirstNum = 10;
         Integer intSecondNum = 50;
         System.out.println("Первое число: " + intFirstNum + "   второе число: " + intSecondNum);
-        Calculator intCalculator = new Calculator(intFirstNum, intSecondNum);
-        System.out.println("Сумма равна " + intCalculator.getSum());
-        System.out.println("Разница равна " + intCalculator.getDiff());
-        System.out.println("Произведение равно " + intCalculator.getMul());
-        System.out.println("Частное равно " + intCalculator.getDiv());
+        System.out.println("Сумма равна " + calculator.getSum(intFirstNum,intSecondNum));
+        System.out.println("Разница равна " + calculator.getDif(intFirstNum,intSecondNum));
+        System.out.println("Произведение равно " + calculator.getMul(intFirstNum,intSecondNum));
+        System.out.println("Частное равно " + calculator.getDiv(intFirstNum,intSecondNum));
 
-        //  Числа типа Long
+//  Числа типа Long
         System.out.println();
         Long longFirstNum = 3000000L;
         Long longSecondNum = -2000L;
         System.out.println("Первое число: " + longFirstNum + "   второе число: " + longSecondNum);
-        Calculator longCalculator = new Calculator(longFirstNum, longSecondNum);
-        System.out.println("Сумма равна " + longCalculator.getSum());
-        System.out.println("Разница равна " + longCalculator.getDiff());
-        System.out.println("Произведение равно " + longCalculator.getMul());
-        System.out.println("Частное равно " + longCalculator.getDiv());
+        System.out.println("Сумма равна " + calculator.getSum(longFirstNum, longSecondNum));
+        System.out.println("Разница равна " + calculator.getDif(longFirstNum, longSecondNum));
+        System.out.println("Произведение равно " + calculator.getMul(longFirstNum, longSecondNum));
+        System.out.println("Частное равно " + calculator.getDiv(longFirstNum, longSecondNum));
 
-        //  Числа типа Double
+//  Числа типа Double
         System.out.println();
         Double doubleFirstNum = 1000.2;
         Double doubleSecondNum = 50.7;
         System.out.println("Первое число: " + doubleFirstNum + "   второе число: " + doubleSecondNum);
-        Calculator doubleCalculator = new Calculator(doubleFirstNum, doubleSecondNum);
-        System.out.println("Сумма равна " + doubleCalculator.getSum());
-        System.out.println("Разница равна " + doubleCalculator.getDiff());
-        System.out.println("Произведение равно " + doubleCalculator.getMul());
-        System.out.println("Частное равно " + doubleCalculator.getDiv());
+        System.out.println("Сумма равна " + calculator.getSum(doubleFirstNum, doubleSecondNum));
+        System.out.println("Разница равна " + calculator.getDif(doubleFirstNum, doubleSecondNum));
+        System.out.println("Произведение равно " + calculator.getMul(doubleFirstNum, doubleSecondNum));
+        System.out.println("Частное равно " + calculator.getDiv(doubleFirstNum, doubleSecondNum));
 
-        //  Ввод чисел с клавиатуры
+//  Ввод чисел с клавиатуры
         System.out.println();
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите первое число");
         if (scanner.hasNextInt()) {
             int number = scanner.nextInt();
-            intCalculator.setFirstNumber(number);
+            calculator.setFirstNumber(number);
         } else {
             System.out.println("Извините, это не целое число. Перезапустите программу и попробуйте снова!");
             return;
         }
-        //intCalculator.setFirstNumber(scanner.nextInt());
+        //intcalculator.setFirstNumber(scanner.nextInt());
         System.out.println("Введите второе число");
         if (scanner.hasNextInt()) {
             int number = scanner.nextInt();
-            intCalculator.setSecondNumber(number);
+            calculator.setSecondNumber(number);
         } else {
             System.out.println("Извините, это не целое число. Перезапустите программу и попробуйте снова!");
             return;
         }
-        //intCalculator.setSecondNumber(scanner.nextInt());
-        System.out.println("Сумма равна " + intCalculator.getSum());
-        System.out.println("Разница равна " + intCalculator.getDiff());
-        System.out.println("Произведение равно " + intCalculator.getMul());
-        System.out.println("Частное равно " + intCalculator.getDiv());
+        //intcalculator.setSecondNumber(scanner.nextInt());
+        System.out.println("Сумма равна " + calculator.getSum());
+        System.out.println("Разница равна " + calculator.getDif());
+        System.out.println("Произведение равно " + calculator.getMul());
+        System.out.println("Частное равно " + calculator.getDiv());
 
         System.out.println();
         Conditioner conditioner1 = new Conditioner("ROYAL Clima", "Tessoro RM-TS17CH-E");
